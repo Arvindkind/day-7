3fb74abff08850e53a77e02bf079fd17e75c3be2
Arvindkind <arvindkuamaur8241@gmail.com>
2024-08-11 00:09:20 +0100

    sitex change

--- index.js
+++ index.js
@@ -260,7 +260,7 @@ console.log(mindd("hello", 29, 5, 2, 5, 3, 3, 734));
 // ! distructuring ka use ak array multipal value ko kisi or variabal me store karne ke liye use karte hai ex
 
 // ? ex1
-let dist = ["tony" ,"bruce" ,"peter" ,"steve","thor"];
+let dist = ["tony", "bruce", "peter", "steve", "thor"];
 
 let winner = dist[0];
 let luser = dist[1];
@@ -270,18 +270,18 @@ console.log(luser);
 
 //? ex2
 
-let dist2 = ["tony" ,"bruce" ,"peter" ,"steve","thor"];
+let dist2 = ["tony", "bruce", "peter", "steve", "thor"];
 
-let [winner2 ,looser2] = dist2;
+let [winner2, looser2] = dist2;
 
 console.log(winner2);
 console.log(looser2);
 
 //? ex3
 
-let dist3 = ["tony" ,"bruce" ,"peter" ,"steve","thor"];
+let dist3 = ["tony", "bruce", "peter", "steve", "thor"];
 
-let [winner3 , looser3 , ...other] = dist3;
+let [winner3, looser3, ...other] = dist3;
 
 console.log(winner3);
 console.log(looser3);
@@ -292,93 +292,96 @@ console.log(other); // store in form of array [ "peter", "steve", "thor" ]
 //? ex
 
 const students = {
-name:"amit",
-class:9,
-age:14,
-subject:["hindi","english","math"],
-username:"karan@123",
-password:"1o3o03pe"
-}
+  name: "amit",
+  class: 9,
+  age: 14,
+  subject: ["hindi", "english", "math"],
+  username: "karan@123",
+  password: "1o3o03pe",
+};
 
-let {username,password,...others } = students;
+let { username, password, ...others } = students;
 console.log(username);
-console.log(others); // object ki traha store hoga{ name: "amit", class: 9, age: 14, subject: (3) […] } 
+console.log(others); // object ki traha store hoga{ name: "amit", class: 9, age: 14, subject: (3) […] }
 
-// ? ex2 
+// ? ex2
 const students2 = {
-  name:"amit",
-  class:9,
-  age:14,
-  subject:["hindi","english","math"],
-  username:"karan@123",
-  password:"1o3o03pe"
-  }
+  name: "amit",
+  class: 9,
+  age: 14,
+  subject: ["hindi", "english", "math"],
+  username: "karan@123",
+  password: "1o3o03pe",
+};
 
-  let {username:user, password:pass} = students2;
+let { username: user, password: pass } = students2;
 
-  console.log(user);
-  console.log(pass);
-  // ? console.log(username); //error username is not defined dikaye ga kyu ki key change kar diya hai
-  
-  //? ex3
+console.log(user);
+console.log(pass);
+// ? console.log(username); //error username is not defined dikaye ga kyu ki key change kar diya hai
 
-  // const students3 = {
-  //   name:"amit",
-  //   class:9,
-  //   age:14,
-  //   subject:["hindi","english","math"],
-  //   username:"karan@123",
-  //   password:"1o3o03pe"
-  //   }
+//? ex3
 
-  //   let {name:names, class:classs, city:place = "mumbai"} = students3;
+// const students3 = {
+//   name:"amit",
+//   class:9,
+//   age:14,
+//   subject:["hindi","english","math"],
+//   username:"karan@123",
+//   password:"1o3o03pe"
+//   }
 
-  //   console.log(names);
-  //   console.log(place);
-    // ? console.log(city); //error city is not defined kyu ki city ki jagha par place kar diye hai
+//   let {name:names, class:classs, city:place = "mumbai"} = students3;
+
+//   console.log(names);
+//   console.log(place);
+// ? console.log(city); //error city is not defined kyu ki city ki jagha par place kar diye hai
 
 // ? parctic qustion 1
 // ?Square and sum the array elements using the arrow function and then find the average of the array
 
-  let arrsss = [2,5,3,4,2,3,3,];
+let arrsss = [2, 5, 3, 4, 2, 3, 3];
 
-  let square = arrsss.map((num) => num*num);
-  console.log(square)
-  let sumsss = square.reduce((sum,el) => sum+el);
-  let avarage = sumsss/arr.length;
-  console.log(avarage);
-
+let square = arrsss.map((num) => num * num);
+console.log(square);
+let sumsss = square.reduce((sum, el) => sum + el);
+let avarage = sumsss / arr.length;
+console.log(avarage);
 
 // ? parctic qustion 2
 // ?Create a new array using the map function whose each element is equal to the original element plus 5.
 
-let arrid = [39,3,4,7,7,4,454,455,];
+let arrid = [39, 3, 4, 7, 7, 4, 454, 455];
 
-let newarr = arrid.map((num) => num+5);
+let newarr = arrid.map((num) => num + 5);
 console.log(newarr);
 
 // ? parctic qustion 3
 //? Create a new array whose elements are in uppercase of words present in the original array
 
-let wordArr = ["adam","peter","bruce","stevs"];
+let wordArr = ["adam", "peter", "bruce", "stevs"];
 
 console.log(wordArr.map((el) => el.toUpperCase()));
 
-function doubleds (...arg){
-  let double = arg.map((ml) => ml*2);
+function doubleds(...arg) {
+  let double = arg.map((ml) => ml * 2);
   console.log(double);
 }
 
-doubleds(3,6,3,5,23,);
+doubleds(3, 6, 3, 5, 23);
 
-let doubleAndReturn = (arg ,...args) => [...arg,...args.map((mul) => mul*2)];
+let doubleAndReturn = (arg, ...args) => [...arg, ...args.map((mul) => mul * 2)];
 
-console.log(doubleAndReturn([2,3,5,6],3,5,6,7)); //Array(8) [ 2, 3, 5, 6, 6, 10, 12, 14 ]
+console.log(doubleAndReturn([2, 3, 5, 6], 3, 5, 6, 7)); //Array(8) [ 2, 3, 5, 6, 6, 10, 12, 14 ]
 
 // ? parctic qustion 3
 //? . Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 
+let mergeObjects = (object, mergeObjects) => ({ ...object, ...mergeObjects });
 
-let mergeObjects = (object , mergeObjects) => ({...object,...mergeObjects});
-
-console.log(mergeObjects({name:"arvind",sirName:"kumar"},{city:"noyda",sector:"33"}));
+console.log(
+  mergeObjects(
+    { name: "arvind", sirName: "kumar" },
+    { city: "noyda", sector: "33" }
+  )
+);
